18440607571fd90b96c7d46c5cd569334d9eb88f
HarrowinG <vladislavtetyuev@mail.ru>
2019-12-19 10:57:51 +0300

    indexator

---
+++ Csharp/Classes/Indexator.cs
@@ -0,0 +1,23 @@
+﻿namespace Csharp.Classes
+{
+    public class Indexator
+    {
+        private string[] words = "The quick brown fox".Split();
+
+        //indexator
+        public string this[int wordNum]
+        {
+            get => words[wordNum];
+            set => words[wordNum] = value;
+        }
+
+        public static void Run()
+        {
+            string str = null;
+            var a = str?[0];  //null-conditional call before square brackets
+
+            var obj = new Indexator();
+            var b = obj?[2];
+        }
+    }
+}

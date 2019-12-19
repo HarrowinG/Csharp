dabdf9881538d87865e912de99a246e92dad28b7
HarrowinG <vladislavtetyuev@mail.ru>
2019-12-19 10:24:04 +0300

    Deconstructor

--- Csharp/Constructors/Constructor.cs
+++ Csharp/Constructors/Constructor.cs
@@ -2,5 +2,56 @@
 {
     public class Constructor
     {
+        public readonly int Age;
+        public readonly int Mass;
+
+        public Constructor(int age, int mass)
+        {
+            Age = age;
+            Mass = mass;
+        }
+
+        public void Deconstruct(out int age, out int mass)
+        {
+            age = Age;
+            mass = Mass;
+        }
+
+        public static void Run()
+        {
+            var obj = new Constructor(24, 65);
+            //all methods are the same, need to be named Deconstruct to be used as deconstruct assignment operator
+            (int age, int mass) = obj;
+
+            (var age2, var mass2) = obj;
+
+            var (age3, mass3) = obj;
+
+            int age4, mass4;
+            (age4, mass4) = obj;
+
+            int age5, mass5;
+            obj.Deconstruct(out age5, out mass5);
+
+            obj.Deconstruct(out int age6, out int mass6);
+
+            obj.Deconstruct(out var age7, out var mass7);
+
+            int age8, mass8;
+            obj.DeconstructExtension(out age8, out mass8);
+
+            obj.DeconstructExtension(out int age9, out int mass9);
+
+            obj.DeconstructExtension(out var age10, out var mass10);
+        }
+    }
+
+    public static class ConstructorExtensions
+    {
+        public static void DeconstructExtension(this Constructor obj, out int age, out int mass)
+        {
+            age = obj.Age;
+            mass = obj.Mass;
+        }
     }
 }
